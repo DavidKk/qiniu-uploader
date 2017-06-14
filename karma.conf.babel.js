c63d3274dfb7da105bd10faa1b189fbd963b9bc8
davidjones <qowera@qq.com>
2017-06-14 15:17:02 +0800

    test 5

--- karma.conf.babel.js
+++ karma.conf.babel.js
@@ -43,7 +43,6 @@ export default function (config) {
     singleRun: true,
     colors: true,
     plugins: [
-      'karma-sauce-launcher',
       'karma-phantomjs-launcher',
       'karma-webpack',
       'karma-chai',
@@ -56,58 +55,63 @@ export default function (config) {
   }
 
   if (process.env.COVERAGE) {
-    _.merge(karmaConf, {
-      reporters: [
-        'mocha',
-        'coverage-istanbul'
-      ],
-      coverageIstanbulReporter: {
-        reports: ['lcov', 'text-summary'],
-        fixWebpackSourcePaths: true
-      },
-      webpack: WebpackMerger(webpackConf, {
-        module: {
-          rules: [
-            {
-              test: /\.js$/,
-              enforce: 'post',
-              loader: 'istanbul-instrumenter-loader',
-              include: webpackConf.resolve.modules,
-              exclude: [/node_modules/, /unitest/],
-              query: {
-                esModules: true
-              }
+    karmaConf.reporters = [
+      'mocha',
+      'coverage-istanbul'
+    ]
+
+    karmaConf.coverageIstanbulReporter = {
+      reports: ['lcov', 'text-summary'],
+      fixWebpackSourcePaths: true
+    }
+
+    karmaConf.webpack = WebpackMerger(webpackConf, {
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            enforce: 'post',
+            loader: 'istanbul-instrumenter-loader',
+            include: webpackConf.resolve.modules,
+            exclude: [/node_modules/, /unitest/],
+            query: {
+              esModules: true
             }
-          ]
-        }
-      })
+          }
+        ]
+      }
     })
   }
 
   if (process.env.TRAVIS) {
-    _.merge(karmaConf, {
-      customLaunchers: sauceBrowsers,
-      browsers: Object.keys(sauceBrowsers),
-      browserDisconnectTimeout: 5000,
-      browserNoActivityTimeout: 5000,
-      browserDisconnectTolerance: 10,
-      sauceLabs: {
-        testName: pkg.name,
-        build: `TRAVIS #${process.env.TRAVIS_BUILD_NUMBER} (${process.env.TRAVIS_BUILD_ID})`,
-        tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
-        retryLimit: 3,
-        startConnect: false,
-        recordVideo: false,
-        recordScreenshots: false,
-        options: {
-          'selenium-version': '2.53.0',
-          'command-timeout': 600,
-          'idle-timeout': 600,
-          'max-duration': 5400,
-        }
-      },
-    })
+    process.env.SAUCE_USERNAME = os.environ.SAUCE_USERNAME
+    process.env.SAUCE_ACCESS_KEY = os.environ.SAUCE_ACCESS_KEY
+
+    console.log(process.env.TRAVIS, process.env.SAUCE_USERNAME, process.env.SAUCE_ACCESS_KEY)
+
+    karmaConf.customLaunchers = sauceBrowsers
+    karmaConf.browsers = Object.keys(sauceBrowsers)
+    karmaConf.browserDisconnectTimeout = 5000
+    karmaConf.browserNoActivityTimeout = 5000
+    karmaConf.browserDisconnectTolerance = 10
+    karmaConf.sauceLabs = {
+      testName: pkg.name,
+      build: `TRAVIS #${process.env.TRAVIS_BUILD_NUMBER} (${process.env.TRAVIS_BUILD_ID})`,
+      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
+      retryLimit: 3,
+      startConnect: false,
+      recordVideo: false,
+      recordScreenshots: false,
+      options: {
+        'selenium-version': '2.53.0',
+        'command-timeout': 600,
+        'idle-timeout': 600,
+        'max-duration': 5400,
+      }
+    }
+
+    karmaConf.plugins.push('karma-sauce-launcher')
   }
 
   config.set(karmaConf)
-};
+}
