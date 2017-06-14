af3b6aea8610ce38bfc42b5f4cf826a6974c41b7
davidjones <qowera@qq.com>
2017-06-14 23:35:13 +0800

    test

--- saucelabs.browser.conf.js
+++ saucelabs.browser.conf.js
@@ -1,46 +1,46 @@
 export const launchers = {
-  sl_chrome: {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    version: '48'
-  },
-  sl_firefox: {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    version: '44'
-  },
-  sl_mac_safari: {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.10',
-    version: '8'
-  },
-  /**
-   * IE 全家桶
-   */
-  sl_win_7_ie_10: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 7',
-    version: '10'
-  },
-  sl_win_8_ie_10: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 8',
-    version: '10'
-  },
-  sl_win_10_ie_11: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 10',
-    version: '11'
-  },
-  sl_win_10_edge: {
-    base: 'SauceLabs',
-    browserName: 'microsoftedge',
-    platform: 'Windows 10'
-  },
+  // sl_chrome: {
+  //   base: 'SauceLabs',
+  //   browserName: 'chrome',
+  //   version: '48'
+  // },
+  // sl_firefox: {
+  //   base: 'SauceLabs',
+  //   browserName: 'firefox',
+  //   version: '44'
+  // },
+  // sl_mac_safari: {
+  //   base: 'SauceLabs',
+  //   browserName: 'safari',
+  //   platform: 'OS X 10.10',
+  //   version: '8'
+  // },
+  // /**
+  //  * IE 全家桶
+  //  */
+  // sl_win_7_ie_10: {
+  //   base: 'SauceLabs',
+  //   browserName: 'internet explorer',
+  //   platform: 'Windows 7',
+  //   version: '10'
+  // },
+  // sl_win_8_ie_10: {
+  //   base: 'SauceLabs',
+  //   browserName: 'internet explorer',
+  //   platform: 'Windows 8',
+  //   version: '10'
+  // },
+  // sl_win_10_ie_11: {
+  //   base: 'SauceLabs',
+  //   browserName: 'internet explorer',
+  //   platform: 'Windows 10',
+  //   version: '11'
+  // },
+  // sl_win_10_edge: {
+  //   base: 'SauceLabs',
+  //   browserName: 'microsoftedge',
+  //   platform: 'Windows 10'
+  // },
   /**
    * Android 全家桶
    */
