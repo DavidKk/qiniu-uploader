928a0ed9f7a8e1fb6125e3cd0aaaf595e4e20b45
davidjones <qowera@qq.com>
2017-06-14 19:29:59 +0800

    添加 android iOS 兼容性

--- saucelabs.browser.conf.js
+++ saucelabs.browser.conf.js
@@ -36,9 +36,63 @@ export const launchers = {
     platform: 'Windows 10',
     version: '11'
   },
-	sl_win_10_edge: {
-		base: 'SauceLabs',
-		browserName: 'microsoftedge',
-		platform: 'Windows 10',
-	},
+  sl_win_10_edge: {
+    base: 'SauceLabs',
+    browserName: 'microsoftedge',
+    platform: 'Windows 10'
+  },
+  /**
+   * Android 全家桶
+   */
+  'sl_android_4.4': {
+    base: 'SauceLabs',
+    browserName: 'android',
+    platform: 'Linux',
+    version: '4.4'
+  },
+  'sl_android_5.0': {
+    base: 'SauceLabs',
+    browserName: 'android',
+    platform: 'Linux',
+    version: '5.0'
+  },
+  'sl_android_5.1': {
+    base: 'SauceLabs',
+    browserName: 'android',
+    platform: 'Linux',
+    version: '5.1'
+  },
+  'sl_android_6.0': {
+    base: 'SauceLabs',
+    browserName: 'android',
+    platform: 'Linux',
+    version: '6.0'
+  },
+  /**
+   * IOS 全家桶
+   */
+  'sl_ios_7.1': {
+    base: 'SauceLabs',
+    browserName: 'iphone',
+    platform: 'OS X 10.10',
+    version: '7.1'
+  },
+  'sl_ios_8.4': {
+    base: 'SauceLabs',
+    browserName: 'iphone',
+    platform: 'OS X 10.10',
+    version: '8.4'
+  },
+  'sl_ios_9.3': {
+    base: 'SauceLabs',
+    browserName: 'iphone',
+    platform: 'OS X 10.10',
+    version: '9.3'
+  },
+  'sl_ios_10.0': {
+    base: 'SauceLabs',
+    browserName: 'iphone',
+    platform: 'OS X 10.10',
+    version: '10.3'
+  }
 }
