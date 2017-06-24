1912fd861e078bc9f38375446feb6f6f39b9cfc6
davidjones <qowera@qq.com>
2017-06-24 14:41:31 +0800

    移除文件

--- webpack.example.config.babel.js
+++ webpack.example.config.babel.js
@@ -5,5 +5,7 @@ import webpackConf from './webpack.develop.config.babel'
 export default WebpackMerger(webpackConf, {
   output: {
     path: path.join(__dirname, './demo'),
+    publicPath: '/qiniu-uploader/',
+    umdNamedDefine: false,
   },
 })
